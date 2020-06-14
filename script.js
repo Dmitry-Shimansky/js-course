461f36711b16ba875ec7e60a65194ac2be9b09ca
Dmitry Shimansky <Shimansky.Dmitry@gmail.com>
2020-06-14 20:50:23 +0300

    Урок 2

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-var money;
-var income;
-var addExpenses;
-var deposit;
-var mission;
-var period;
+var money = 1000;
+var income = 'Фриланс';
+var addExpenses = 'Интернет, Такси, Коммуналка, социалка';
+var deposit = true;
+var mission = 18489;
+var period = 12;
 
-console.log ('Сообщение для консоли');
-alert ('Хороший урок');
+console.log (typeof money);
+console.log (typeof income);
+console.log (typeof deposit);
+
+console.log (addExpenses.length);
+
+console.log ('Период равен ' + period + ' месяцев');
+console.log ('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+
+console.log (addExpenses.toLowerCase());
+console.log (addExpenses.split(', '));
+
+let budgetDay;
+budgetDay = money/30;
+console.log (budgetDay);
